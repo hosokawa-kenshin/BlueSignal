cd007bb8369ab24ba2035471180fc82f517d4dbf
hosokawa-kenshin <pnl84lku@s.okayama-u.ac.jp>
2024-12-04 14:17:00 +0900

    Add periferal application

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flutter_ble_peripheral/flutter_ble_peripheral_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterBlePeripheralPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterBlePeripheralPluginCApi"));
 }
